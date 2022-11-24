5bfbea27ef492c3bebb894e45a2670d99929a7c5
Ammubhuvana <bhuvanaarunasalam@gmail.com>
2022-11-23 21:07:58 -0800

    This is my Library application using springboot in java

---
+++ StudentProject/src/hashmap.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class hashmap {
+
+
+
+
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		s=s.replace(" ","" );
+		char arr[]=s.toCharArray();
+		int count=0;
+		
+		for(int i=0;i<arr.length;i++) {
+			count=0;
+			for(int j=0;j<arr.length;j++) {
+				if(arr[i]==arr[i]) {
+					count++;
+				}
+			}
+		
+		}
+	}
+}
+		
+		
+		
+		
+
+	
+
+
